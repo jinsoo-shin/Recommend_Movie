d7e868d8f90bed279ce90b073d3d5fca8c178142
신진수 <sjins0127@gmail.com>
2019-09-18 09:19:43 +0900

    knn user-based 구현, 데이터 전처리 정확도 높이는 작업 필요

--- backend/api/views/js_view.py
+++ backend/api/views/js_view.py
@@ -17,7 +17,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 from sklearn.mixture import GaussianMixture
 
-from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import KNeighborsRegressor
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 import sqlite3
@@ -36,50 +36,42 @@ def test(request):
     if request.method == 'GET':
 
         cnx = sqlite3.connect('db.sqlite3')
-        df_movies = pd.read_sql_query("SELECT id movieid,title FROM api_movie",cnx)
-        df_ratings = pd.read_sql_query("SELECT id userid,movieid,rating FROM api_rating LIMIT 100000",cnx)
+        df = pd.read_sql_query("SELECT r.userid, r.rating,p.gender,p.age,p.occupation FROM api_profile p,api_rating r WHERE p.id = r.userid and r.movieid=1197 LIMIT 10",cnx)
 
-        # print(df_movies.head())
-        # print(df_ratings.head())
+        print(df.head())
+
+        print(df['gender'].value_counts())
+
+        df['gender']=df['gender'].map({
+            'F':0,
+            'M':1
+        })
+
+        print(df.head())
+
+        df['occupation'] = df['occupation'].map({
+            "other":0, "academic/educator":1, "artist":2,"clerical/admin":3, "college/grad student":4,
+           "customer service":5,  "doctor/health care":6,"executive/managerial":7,"farmer":8,  "homemaker":9,
+             "K-12 student":10, "lawyer":11, "programmer":12, "retired":13, "sales/marketing":14,
+             "scientist":15,  "self-employed":16,  "technician/engineer":17, "tradesman/craftsman":18,
+             "unemployed":19,  "writer":20
+        })
+        print(df.head())
+
+        y=df['rating'].values.reshape(-1,1)
+        X=df.loc[:,['userid','gender','age','occupation']]
+
+        kNN = KNeighborsRegressor(n_neighbors=2)
+
+        print(kNN.fit(X,y))
+        example = np.array([1,0,1,10])
+        example = example.reshape(1, -1)
+        test = kNN.fit(X,y).predict(example)
+
+        print(test)
+        # print("ddd",df.loc[:,['userid','gender','age','occupation']])
         # 대상 영화와 데이터베이스의 다른 모든 영화 사이의 "거리"를 계산 한 다음 거리를 평가하고
         # 가장 가까운 K 개의 가장 가까운 이웃 영화를 가장 유사한 영화 추천으로 반환
-
-
-        #######################
-
-        #######################
-
-        df_movie_features= df_ratings.pivot(
-            index='movieid',
-            columns='userid',
-            values='rating'
-        ).fillna(0)
-
-        print(df_movie_features)
-        # row가 movieid, col이 userid
-        # userid    1   2    3   4
-        # movieid
-        # 1        3    4   2   4.5
-
-        print(df_movie_features.index)
-        print(df_movie_features.columns)
-        print(df_movie_features.values[0:5])
-
-        #변환 행렬
-        mat_movie_features = csr_matrix(df_movie_features.values)
-        print(mat_movie_features)
-
-        model_knn = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=20, n_jobs=-1)
-        print(model_knn.fit(mat_movie_features))
-
-        distances, indices = model_knn.kneighbors(df_movie_features.reshape(1,-1),n_neighbors=20)
-
-        for i in range(len(distances.flatten())):
-            print('{0}: {1}, with distance  of {2}'.format(i,df_movie_features.index[indices.flattens()[i]],distances.flattens()[i]))
-        #######################
-        # sparse_ratings = csr_matrix(pd.SparseDataFrame(mat_movie_features).to_coo())
-        # print(sparse_ratings)
-
 
 
         request_data=[]
