f14abb9a7a3611d08b94078051cee4d8fdc40be9
신진수 <sjins0127@gmail.com>
2019-09-20 17:28:58 +0900

    kNN 알고리즘을 사용하여 추천 영화 id 10개 추천

--- backend/api/views/js_view.py
+++ backend/api/views/js_view.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import NearestNeighbors
 import sqlite3
 from sklearn.preprocessing import MinMaxScaler
 
@@ -35,22 +36,22 @@ def test(request):
             'F':0,
             'M':1
         })
-        print(df_user.head())
+        # print(df_user.head())
 
         query = "select userid, movieid, rating from api_rating where rating>=4 and userid != "+str(userid)+" and movieid in (select movieid from api_rating where rating>=4 and userid="+str(userid)+" ) order by movieid asc"
         df_rating = pd.read_sql_query(query, cnx)
-        print(df_rating.head())
+        # print(df_rating.head())
         #각각의 영화를 본 유저들의 rating을 가져오기
 
 
         dfdf = df_rating.pivot_table(index="userid",columns="movieid",values="rating",fill_value=0)
-        print(dfdf.head())
+        # print(dfdf.head())
 
         df_rating_flat = pd.DataFrame(dfdf.to_records())
 
         df_new = pd.merge(df_user,df_rating_flat,on="userid")
 
-        print("rating이랑 유저정보 합체",df_new)
+        # print("rating이랑 유저정보 합체",df_new)
 
         col_list = list(df_new.columns.values)[1:]  #정규화를 위해 사용합니다
 
@@ -58,9 +59,7 @@ def test(request):
         X=df_new.loc[:,col_list]
         output = MinMaxScaler().fit_transform(X)
         X= pd.DataFrame(output)
-        print(X)
-        kNN = KNeighborsClassifier(n_neighbors=5)
-
+        # print(X)
         #gender, age, 봤던 영화 리스트~~~~~
         user = my_sql("user",str(userid))
 
@@ -70,15 +69,32 @@ def test(request):
         for i in range(len(movie_list_tmp)):
             my_user_list.append(movie_list_tmp[i][0])
 
-        print(my_user_list)
+        # print(my_user_list)
 
         example = np.array(my_user_list)
         example = example.reshape(1, -1)
-        test = kNN.fit(X,y).predict(example)
 
-        print(test)
 
-        print(y[test])
+        # kNN = KNeighborsClassifier(n_neighbors=5)
+        # test = kNN.fit(X,y).predict(example)
+        # print(test)
+        # print(y[test])
+
+        nN= NearestNeighbors(n_neighbors=5,metric="euclidean").fit(X,y)
+        nN_predict =nN.kneighbors_graph(example).toarray()
+
+        similar_user= []
+        for i in range(len(nN_predict[0])):
+            if nN_predict[0][i] == 1.0:
+                similar_user.append(str(y[i][0]))
+                print(y[i][0])
+        print("유사한 유저 id",similar_user)
+
+        user_list=",".join(similar_user)
+
+        query = "select distinct(movieid) mid, m.title from api_rating r, api_movie m where mid = m.id and userid in ("+user_list+") and r.rating>=4 and movieid not in ( select movieid from api_rating where userid=" + str(userid)+") order by r.rating desc limit 10"
+        result = pd.read_sql_query(query, cnx)
+        print(result.head())
 
 
         # df_test = pd.DataFrame(index=df_user,columns=df_movie)
@@ -86,65 +102,7 @@ def test(request):
         # sql = "Select user_id,gender,age,occupation from api_profile where gender= '"+ str(user[0])+ "' and age = "+ str(user[1])+" and not user_id= "+str(userid)
 
 #################################
-        # userid = 2
-        # user = my_sql("user",str(userid))
-        # cnx = sqlite3.connect('db.sqlite3')
-        # sql = "Select user_id,gender,age,occupation from api_profile where gender= '"+ str(user[0])+ "' and age = "+ str(user[1])+" and not user_id= "+str(userid)
-        # # sql = "Select user_id,gender,age,occupation from api_profile where gender= '"+ str(user[0])+ "' and age = "+ str(user[1])+" and occupation= '"+str(user[2])+"' and not user_id= "+str(userid)
-        #
-        # print(pd.read_sql_query(sql,cnx))
-        #
-        # df = pd.read_sql_query(sql,cnx)
-        #
-        # addRownum =[]
-        # for i in range(len(df)):
-        #     addRownum.append(i+1)
-        #
-        # df['rownum']=addRownum
-        #
-        # df['gender']=df['gender'].map({
-        #     'F':2,
-        #     'M':1
-        # })
-        #
-        # # print(df.head())
-        # df['occupation'] = df['occupation'].map({
-        #     "other":21, "academic/educator":1, "artist":2,"clerical/admin":3, "college/grad student":4,
-        #    "customer service":5,  "doctor/health care":6,"executive/managerial":7,"farmer":8,  "homemaker":9,
-        #      "K-12 student":10, "lawyer":11, "programmer":12, "retired":13, "sales/marketing":14,
-        #      "scientist":15,  "self-employed":16,  "technician/engineer":17, "tradesman/craftsman":18,
-        #      "unemployed":19,  "writer":20
-        # })
-        #
-        # # df=df.sample(frac=1)
-        # # df=df.sample(frac=1).reset_index(drop=True)
-        # print(df.head())
-        #
-        # # output = MinMaxScaler.transform(df)
-        # # output = pd.DataFrame(output, columns=df.columns, index=list(df.index.values))
-        #
-        #
-        #
-        # y=df['rownum'].values.reshape(-1,1)
-        # # y=df['user_id'].values.reshape(-1,1)
-        # X=df.loc[:,['gender','age','occupation']]
-        #
-        # output = MinMaxScaler().fit_transform(X)
-        # X= pd.DataFrame(output)
-        # print(X)
-        #
-        #
-        # kNN = KNeighborsClassifier(n_neighbors=5)
-        # # kNN = KNeighborsRegressor(n_neighbors=5)
-        #
-        # print(kNN.fit(X,y))
-        # example = np.array([dict_gender[user[0]],user[1],dict_occupation[user[2]]])
-        # # example = np.array([0,1,10])
-        # example = example.reshape(1, -1)
-        # test = kNN.fit(X,y).predict(example)
-        #
-        # print(test)
-        # # print("ddd",df.loc[:,['userid','gender','age','occupation']])
+
         # # 대상 영화와 데이터베이스의 다른 모든 영화 사이의 "거리"를 계산 한 다음 거리를 평가하고
         # # 가장 가까운 K 개의 가장 가까운 이웃 영화를 가장 유사한 영화 추천으로 반환
 
